511d6726cf731119b391f1f2a2d7a8f16747e468
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-11-02 17:46:16 -0600

    added semantic

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Review from './Review'
-
+import {Card} from 'semantic-ui-react'
 class Beer extends Component {
 
     state = {
@@ -14,13 +14,17 @@ class Beer extends Component {
 
     render(){
         return(
-            <div>
-            <p>{this.props.beer.name}</p>
+        <Card
+            image={this.props.beer.imageUrl}
+            header={this.props.beer.name}
+            
+            description={this.state.showReview ? <Review review={this.props.beer.review}/> : '' }
+            {/* <h3>{this.props.beer.name}</h3>
             <img src={this.props.beer.imageUrl} />
             <p>{this.props.beer.abv}</p>
             <button onClick={this.showThatReview}>{this.state.showReview ? 'Hide Review' : 'Show Review'}</button>
-            {this.state.showReview ? <Review review={this.props.beer.review}/> : '' }
-            </div> 
+            {this.state.showReview ? <Review review={this.props.beer.review}/> : '' } */}
+        /> 
 
         )
     }
--- src/components/Cooler.js
+++ src/components/Cooler.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Beer from './Beer'
+import { Container } from 'semantic-ui-react'
 
 const Cooler = ({beer}) => {
 
     return beer.map((beer,i) => {
         return (
+            <Container>
             <Beer key={i} beer={beer} />
+            </Container>
         )
     })
 }
