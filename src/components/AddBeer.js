2c36c6db593acca78887014e2ef77c149016f5c9
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-11-05 09:00:09 -0700

    changed review to a text area

--- src/components/AddBeer.js
+++ src/components/AddBeer.js
@@ -1,4 +1,4 @@
-import {Button, Form, Segment} from 'semantic-ui-react'
+import {Button, Form, Segment, TextArea} from 'semantic-ui-react'
 import React from 'react'
 
 
@@ -8,10 +8,12 @@ const AddBeer = ({getName, getAbv, getImg, getReview, submitForm}) => {
         <Segment inverted>
         <Form inverted>
             <Form.Group widths='equal'>
+            <Form.Field>
                 <Form.Input required onChange={getName} fluid label='Beer Name' placeholder='Beer name' />
                 <Form.Input required onChange={getImg} fluid label='Image' placeholder='Image' />
                 <Form.Input required onChange={getAbv} type='number' fluid label='ABV' placeholder='ABV'/>
-                <Form.Input required onChange={getReview} fluid label='Review' placeholder='Write a review' />
+            </Form.Field>
+                <TextArea autoHeight required onChange={getReview} fluid label='Review' placeholder='Write a review' rows={2}/>
             </Form.Group>
             <Button color='green' onClick={submitForm} type='submit'>Submit</Button>
         </Form>
